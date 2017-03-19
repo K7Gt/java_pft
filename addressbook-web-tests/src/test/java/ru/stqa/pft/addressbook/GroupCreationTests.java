663fe8dbef4cac72485d36e549b5e75e90b8a0d3
Go1em <gojiem777@gmail.com>
2017-03-19 12:49:28 +0300

    hometask_4-intermediate step2_fixtures

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -19,12 +19,7 @@ public class GroupCreationTests {
     public void setUp() throws Exception {
         FirefoxBinary binary = new FirefoxBinary(new File("c:\\Firefox_for_course\\firefox.exe"));
         wd = new FirefoxDriver(binary, new FirefoxProfile());
-
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }
-    
-    @Test
-    public void GroupCreationTests() {
         wd.get("http://localhost/addressbook/");
         wd.findElement(By.name("user")).click();
         wd.findElement(By.name("user")).clear();
@@ -33,6 +28,11 @@ public class GroupCreationTests {
         wd.findElement(By.name("pass")).clear();
         wd.findElement(By.name("pass")).sendKeys("secret");
         wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+
+    }
+    
+    @Test
+    public void testGroupCreation() {
         wd.findElement(By.linkText("groups")).click();
         wd.findElement(By.name("new")).click();
         wd.findElement(By.name("group_name")).click();
