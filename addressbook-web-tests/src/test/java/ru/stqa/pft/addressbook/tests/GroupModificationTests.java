b9ee60be4ba2e53f36d617dcce61bdcaf2154747
Go1em <gojiem777@gmail.com>
2017-03-25 11:48:28 +0300

    default values

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -13,7 +13,7 @@ public class GroupModificationTests extends TestBase{
         app.getNavigationHelper().gotoGroupPage();
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("test7", "test7", "test7"));
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test7", "test7"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
     }
