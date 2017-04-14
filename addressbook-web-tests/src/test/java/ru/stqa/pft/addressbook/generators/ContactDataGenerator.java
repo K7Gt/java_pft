5697e5388597b1a3e7948f84ede1ebc4058e91c4
Go1em <gojiem777@gmail.com>
2017-04-14 22:31:01 +0300

    changes in according to l6_3

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/ContactDataGenerator.java
@@ -1,5 +1,8 @@
 package ru.stqa.pft.addressbook.generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 import java.io.File;
@@ -12,17 +15,32 @@ import java.util.List;
 /**
  * Created by Golem on 14.04.2017.
  */
-public class ContactDataGenerators {
+public class ContactDataGenerator {
+
+    @Parameter(names = "-c",description = "GroupCount")
+    public int count;
+
+    @Parameter(names = "-f",description = "Target file")
+    public String file;
 
     public static void main(String[] args) throws IOException {
-        int count = Integer.parseInt(args[0]);
-        File file = new File(args[1]);
-
-        List<ContactData> contacts = generateContacts(count);
-        save(contacts,file);
+        ContactDataGenerator generator = new ContactDataGenerator();
+        JCommander jCommander = new JCommander(generator);
+        try{
+            jCommander.parse(args);
+        }catch (ParameterException ex){
+            jCommander.usage();
+            return;
+        }
+        generator.run();
     }
 
-    private static void save(List<ContactData> contacts, File file) throws IOException {
+    private void run() throws IOException {
+        List<ContactData> contacts = generateContacts(count);
+        save(contacts,new File(file));
+    }
+
+    private void save(List<ContactData> contacts, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for(ContactData contact : contacts){
             writer.write(String.format(
@@ -47,7 +65,7 @@ public class ContactDataGenerators {
 
     }
 
-    private static List<ContactData> generateContacts(int count) {
+    private List<ContactData> generateContacts(int count) {
         List<ContactData> contacts = new ArrayList<ContactData>();
         for (int i = 0; i < count; i++) {
             contacts.add(new ContactData().withContactName(String.format("contactName %s",i))
