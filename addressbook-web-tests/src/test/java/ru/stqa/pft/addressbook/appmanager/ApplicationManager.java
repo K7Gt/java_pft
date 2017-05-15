bb6b19fe477b9fde20df578e7bf88b08cef6f14a
Go1em <gojiem777@gmail.com>
2017-05-15 10:21:04 +0300

    changes in according to l10_2-2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
@@ -7,10 +8,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
@@ -37,18 +41,27 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+
         dbHelper = new DbHelper();
-        if(browser.equals(BrowserType.FIREFOX)){
-            FirefoxBinary binary = new FirefoxBinary(new File(properties.getProperty("browser.firefoxFolder")));
-            wd = new FirefoxDriver(binary, new FirefoxProfile());
-            wd.manage().window().maximize();
-            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        } else if(browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        } else if (browser.equals(BrowserType.IE)){
-            wd = new InternetExplorerDriver();
-            wd.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+
+        if("".equals(properties.getProperty("selenium.server"))) {
+
+            if (browser.equals(BrowserType.FIREFOX)) {
+                FirefoxBinary binary = new FirefoxBinary(new File(properties.getProperty("browser.firefoxFolder")));
+                wd = new FirefoxDriver(binary, new FirefoxProfile());
+                wd.manage().window().maximize();
+                wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            } else if (browser.equals(BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+                wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            } else if (browser.equals(BrowserType.IE)) {
+                wd = new InternetExplorerDriver();
+                wd.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
         }
         wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
